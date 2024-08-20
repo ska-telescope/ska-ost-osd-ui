37334d96b405b8819dce3a854c9e8a5b865b247b
Omprakash Verma <omprakash_verma@persistent.com>
2024-08-20 20:06:24 +0530

    NAK-996 added workaround fix

--- src/services/apis.tsx
+++ src/services/apis.tsx
@@ -26,7 +26,7 @@ function handleAxiosError(error: object) {
 }
 
 const apiService = {
-  baseURL: () => window.env.BACKEND_URL,
+  baseURL: () => 'http://127.0.0.1:6001/ska-oso-slt-services/slt/api/v0',
 
   postShiftData: async (path, shiftData: any): Promise<any> => {
     const baseUrl = apiService.baseURL();
