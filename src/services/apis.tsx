ad900052a0f80333e53aff17bc9a3ec5c624c2b7
Omprakash Verma <omprakash_verma@persistent.com>
2024-08-22 15:53:55 +0530

    NAK-996 Updated code

--- src/services/apis.tsx
+++ src/services/apis.tsx
@@ -26,7 +26,9 @@ function handleAxiosError(error: object) {
 }
 
 const apiService = {
-  baseURL: () => 'http://192.168.49.2:30001/ska-oso-slt-services/slt/api/v0',
+  // 
+  baseURL: () => 'https://k8s.stfc.skao.int/dev-ska-oso-slt-services-nak-998-merge-image-api/slt/api/v0',
+  // baseURL: () => 'http://192.168.49.2:30001/ska-oso-slt-services/slt/api/v0',
   // baseURL: () => 'http://127.0.0.1:6001/ska-oso-slt-services/slt/api/v0',
   postShiftData: async (path, shiftData: any): Promise<any> => {
     const baseUrl = apiService.baseURL();
