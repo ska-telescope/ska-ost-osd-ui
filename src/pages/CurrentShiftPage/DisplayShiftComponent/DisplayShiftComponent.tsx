4c2a65bc09d4422b50328403f4beac3d8c0418c1
Omprakash Verma <omprakash_verma@persistent.com>
2025-01-06 16:51:52 +0530

    NAK-1082 Added support for local data

--- src/pages/CurrentShiftPage/DisplayShiftComponent/DisplayShiftComponent.tsx
+++ src/pages/CurrentShiftPage/DisplayShiftComponent/DisplayShiftComponent.tsx
@@ -218,6 +218,7 @@ function DisplayShiftComponent() {
   };
 
   useEffect(() => {
+    // eslint-disable-next-line no-console
     console.log('window.env.BACKEND_URL',window.env.BACKEND_URL,window.env.REACT_APP_USE_LOCAL_DATA)
     fetchSltCurrentShifts();
     // updateShiftLogs();
