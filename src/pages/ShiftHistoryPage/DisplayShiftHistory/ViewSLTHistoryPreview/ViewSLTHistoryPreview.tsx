c8be5fbbc63f3cdfab357f504862660520ead202
Omprakash Verma <omprakash_verma@persistent.com>
2025-01-07 11:39:29 +0530

    NAK-1082 Update code to use data from files

--- src/pages/ShiftHistoryPage/DisplayShiftHistory/ViewSLTHistoryPreview/ViewSLTHistoryPreview.tsx
+++ src/pages/ShiftHistoryPage/DisplayShiftHistory/ViewSLTHistoryPreview/ViewSLTHistoryPreview.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PreviewIcon from '@mui/icons-material/Preview';
 import apiService from '../../../../services/apis';
 import { createShiftPath } from '../../../../utils/api_constants';
+import SHIFT_DATA_LIST from '../../../../DataModels/DataFiles/shiftDataList';
+import { USE_LOCAL_DATA } from '../../../../utils/constants';
 
 interface EntryFieldProps {
   shiftData;
@@ -9,7 +11,16 @@ interface EntryFieldProps {
 }
 
 const ViewSLTHistory = ({ shiftData, updatedList }: EntryFieldProps) => {
+  const useLocalData = () => {
+    const result = SHIFT_DATA_LIST.filter((item) => item.shift_id === shiftData.shift_id);
+    updatedList(result[0]);
+  };
+
   const fetchSltHistoryByID = async () => {
+    if (USE_LOCAL_DATA) {
+      useLocalData();
+      return true;
+    }
     const path = createShiftPath(shiftData.shift_id, 'id');
     const response = await apiService.getSltData(path);
     if (response.status === 200 && response.data && response.data.length > 0) {
