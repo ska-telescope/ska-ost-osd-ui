31a1feb799d3fe176f48109e98d375a87d02b7a2
Betelgeusenova <sagar_wankhede1@persistent.com>
2025-01-08 15:43:14 +0530

    NAK-1080 added few test cases

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -9,16 +9,30 @@ describe('<Components />', () => {
     cy.mount(
       <ThemeProvider theme={theme(THEME_DARK)}>
         <CssBaseline />
+
         <App />
       </ThemeProvider>
     );
+    cy.get('body').then((element) => {
+      if (element.find('[testId="footerId"]').length) {
+        cy.get('[data-testid="footerId"]').contains('0.2.0').should('be.visible');
+        cy.get('[data-testid="headerId"]').contains('Shift Log Tool').should('be.visible');
+      }
+    });
   });
   it(`Theme ${THEME_LIGHT}: Renders`, () => {
     cy.mount(
       <ThemeProvider theme={theme(THEME_LIGHT)}>
         <CssBaseline />
+
         <App />
       </ThemeProvider>
     );
+    cy.get('body').then((element) => {
+      if (element.find('[testId="footerId"]').length) {
+        cy.get('[data-testid="footerId"]').contains('0.2.0').should('be.visible');
+        cy.get('[data-testid="headerId"]').contains('Shift Log Tool').should('be.visible');
+      }
+    });
   });
 });
