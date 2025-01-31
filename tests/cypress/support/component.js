28d1f49ef5844d5f2fc50bd046b0a0d1b0f664a3
Betelgeusenova <sagar_wankhede1@persistent.com>
2025-01-31 15:59:19 +0530

    NAK-1141 added support file

--- tests/cypress/support/component.js
+++ tests/cypress/support/component.js
@@ -1,8 +1,12 @@
 import '@cypress/code-coverage/support';
 import './commands';
-
+import 'cypress-file-upload';
 import { mount } from 'cypress/react18';
 
 Cypress.Commands.add('mount', mount);
+
+Cypress.Commands.add('readJsonFile', filePath => {
+    return cy.fixture(filePath);
+  });
 
 Cypress.on('uncaught:exception', () => false);
