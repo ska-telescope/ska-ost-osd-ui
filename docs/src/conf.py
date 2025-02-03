7ec14a62907c4934d84db584ec1d30ece63547f4
Betelgeusenova <sagar_wankhede1@persistent.com>
2025-02-03 20:22:50 +0530

    REL-1940 bumped version to 0.3.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -29,9 +29,9 @@ copyright = '2024, SKAO'
 author = 'SKAO, (Team NAKSHATRA)'
 
 # The short X.Y version
-version = '0.3.0'
+version = '0.3.1'
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 # -- General configuration ---------------------------------------------------
 
