cddabb2be870fd555a15f2523216995cba1363a5
Betelgeusenova <sagar_wankhede1@persistent.com>
2025-02-04 14:21:58 +0530

    NAK-1141 addressed review comments

--- tests/cypress/support/component.js
+++ tests/cypress/support/component.js
@@ -5,8 +5,4 @@ import { mount } from 'cypress/react18';
 
 Cypress.Commands.add('mount', mount);
 
-Cypress.Commands.add('readJsonFile', (filePath) => {
-  return cy.fixture(filePath);
-});
-
 Cypress.on('uncaught:exception', () => false);
