8e38c865bf40a90610e221282c86cce8c733503a
omprakash_verma <omprakash_verma@persistent.com>
2025-02-20 19:34:20 +0530

    NAK-1084 Update make file

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,7 @@ module.exports = () => {
         filename: 'remoteEntry.js',
         remotes: {},
         exposes: {
-          './osd': './src/components/Routing/Routing.tsx',
+          './osd': './src/components/pages/HomePage/HomePage.tsx',
         },
         shared: {
           ...deps,
